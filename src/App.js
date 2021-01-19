187be55775595461a50e3a87516ce4e44f18de8a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-19 18:20:45 +0100

    Add global state in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState } from "react";
+
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Buy milk", day: "Feb 5th at 2:30pm", reminder: true },
+    { id: 2, text: "Buy bread", day: "Feb 15th at 12:30pm", reminder: true },
+    { id: 3, text: "Buy eggs", day: "Feb 25th at 5:30pm", reminder: true },
+  ]);
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
