31a6fddb7b97af752cb84180189e9b3838f4eeec
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-19 20:05:13 +0100

    Edit toggle add/close btn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     { id: 1, text: "Buy milk", day: "Feb 5th at 2:30pm", reminder: true },
     { id: 2, text: "Buy bread", day: "Feb 15th at 12:30pm", reminder: true },
@@ -31,8 +32,12 @@ function App() {
 
   return (
     <div className='container'>
-      <Header title='Task Tracker' />
-      <AddTask onAdd={addTask} />
+      <Header
+        title='Task Tracker'
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
