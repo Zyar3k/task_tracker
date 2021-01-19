d61dceb0f651609c3ffe4fd0d245a8e51df5cd36
Arkadiusz Zygan <a.zygan@gmail.com>
2021-01-19 17:55:25 +0100

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className='container'>
-      <h1>start project</h1>
+      <Header title='Taks Tracker' />
     </div>
   );
 }
